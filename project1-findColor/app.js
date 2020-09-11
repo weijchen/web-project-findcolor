f00f24f75e3a1a1d4c30f4bd26fa104125bb826f
jimmyweicc <weijchen0919@gmail.com>
2020-09-11 13:52:52 +0800

    rebuild project1

---
+++ project1-findColor/app.js
@@ -0,0 +1,18 @@
+const express = require("express"),
+      app     = express(),
+      PORT    = process.env.PORT || 3030;
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(PORT, function (req, res) {
+  console.log(`Server starts at http://localhost:${PORT}`);
+});
